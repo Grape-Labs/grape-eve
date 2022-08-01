81e162538a0e645ab5767aa931916eca753e9d79
Kirk <kfournia@gmail.com>
2022-08-01 14:43:32 +0300

    version bump

---
+++ app/types/pdas.ts
@@ -0,0 +1,21 @@
+import * as anchor from "@project-serum/anchor";
+import {Connection, PublicKey, TransactionInstruction} from "@solana/web3.js";
+
+export const EVE_PROGRAM_ID = new PublicKey("BFWcpbojQhtjeuHCLnrz8fMZzExDD4K7YGdJkgpbug9X")
+export const THREAD: string = "THREAD";
+export const COMMUNITY: string = "COMMUNITY";
+
+
+export async function getThread(uuid: string): Promise<[PublicKey, number]> {
+    return PublicKey.findProgramAddress([
+        Buffer.from(anchor.utils.bytes.utf8.encode(THREAD)),
+        Buffer.from(anchor.utils.bytes.utf8.encode(uuid)),
+    ], EVE_PROGRAM_ID);
+}
+
+export async function getCommunity(uuid: string): Promise<[PublicKey, number]> {
+    return PublicKey.findProgramAddress([
+        Buffer.from(anchor.utils.bytes.utf8.encode(COMMUNITY)),
+        Buffer.from(anchor.utils.bytes.utf8.encode(uuid)),
+    ], EVE_PROGRAM_ID);
+}
